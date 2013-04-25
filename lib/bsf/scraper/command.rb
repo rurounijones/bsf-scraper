477dbd917ddefba3060e4eb3d8541d3ab9aa4e59
Jeffrey Jones <jeff@jones.be>
2013-04-25 21:48:36 +0900

    Move argument parsing into Command class.
    The Trollop command-line parsing library allows us to specify
    a variable containing the command-line arguments. Therefore to
    keep the executable code as simple as possible and aid
    testability we will move the command-line parsing into the
    Bsf::Scraper::Command class.
    
    The trollop code was initially in the executable file because
    I did not know about this possibility and thought it was
    hard-coded to parse ARGV

--- lib/bsf/scraper/command.rb
+++ lib/bsf/scraper/command.rb
@@ -1,11 +1,30 @@
+require 'trollop'
+require 'bsf/scraper/version'
+
 module Bsf
   module Scraper
     class Command
-      
-      def initialize(options)
-        raise ArgumentError, "options must be a Hash" unless options.class == Hash
+
+      def initialize(arguments)
+        unless arguments.class == Array
+          raise ArgumentError, "arguments must be an Array" 
+        end
+        @options = Trollop::options arguments do
+          opt :csv_path, "Full path to output csv file (Required)",
+              :type => :string, :short => "-c", :required => true
+          opt :database_host,
+              "Host IP of the database (Only required if connecting via TCP)",
+              :type => :string, :short => "-h"
+          opt :database_name, "Database name (Required)", :type => :string,
+              :short => "-n", :required => true
+          opt :database_user, "Database username (Required)", :type => :string,
+              :short => "-u", :required => true
+          opt :database_password, "Database password (Required)",
+              :type => :string, :short => "-p", :required => true
+          version "Bargain Stock Funds Scraper version #{Bsf::Scraper::VERSION}"
+        end
       end
-     
+
     end
   end
 end
--- spec/bsf/scraper/command_spec.rb
+++ spec/bsf/scraper/command_spec.rb
@@ -3,8 +3,10 @@ require 'bsf/scraper/command'
 describe Bsf::Scraper::Command do
 
   describe '.initialize' do
-    it { expect { described_class.new }.to raise_error(ArgumentError, /0 for 1/) }
-    it { expect { described_class.new("test") }.to raise_error(ArgumentError, /must be a Hash/) }    
+    it { expect { described_class.new }.
+         to raise_error(ArgumentError, /0 for 1/) }
+    it { expect { described_class.new("test") }.
+         to raise_error(ArgumentError, /must be an Array/) }
   end
 
 end
